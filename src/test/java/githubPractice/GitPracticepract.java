0c9cacfb94bb790e0e09551d9c6994f25bb5131f
PC <PC@DESKTOP-FNEC2U3>
2022-11-21 15:20:16 +0530

    practice this

--- src/test/java/githubPractice/GitPracticepract.java
+++ src/test/java/githubPractice/GitPracticepract.java
@@ -3,9 +3,9 @@ package githubPractice;
 import org.testng.annotations.Test;
 
 public class GitPracticepract {
-@Test
-public static void practice() {
-	System.out.println("hi ");
-	
-}
+	@Test
+	public static void practice() {
+		System.out.println("hi ");
+
+	}
 }
